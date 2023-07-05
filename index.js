e53a6af258a97834cbf6b9f5e123da0973298dfb
ifeoma95 <enwezorifeoma3@gmail.com>
2023-07-05 08:28:11 +0100

    counter done

---
+++ index.js
@@ -0,0 +1,62 @@
+const btn = document.querySelector('.btn1');
+
+ 
+
+const timer = document.querySelector('.timer');
+
+ 
+
+const display = document.querySelector('.display');
+
+ 
+
+let count = 0;
+
+let time_counter = 5;
+
+let interval;
+
+ 
+
+btn.addEventListener('click', () => {
+
+  time_counter = 5;
+
+  count++;
+
+  display.innerHTML = count;
+
+  timer.innerHTML = time_counter;
+
+  clearInterval(interval);
+
+  ifeoma();
+
+})
+
+
+
+
+
+function ifeoma() {
+
+  interval = setInterval(() => {
+
+    time_counter--;
+
+    timer.innerHTML = time_counter;
+
+    if (time_counter === 0) {
+
+      clearInterval(interval);
+
+      count = 0;
+
+      display.innerHTML = count;
+
+    }
+
+  }, 1000);
+
+}
+
